9a07ce607b5dc5f29fbf3b569b5775fd4bd033a9
fyrmnzia <1rifqi.firmansyah@gmail.com>
2024-10-02 21:29:52 +0700

    demo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-black text-white font-VT323">
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
